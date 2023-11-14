6abe360fa61463f0f799756f9dc435c877472574
MaltaLiz <lizlrmalta@gmail.com>
2023-11-14 00:25:03 -0300

    funcionalidades: editar task e deletar task

--- src/datebase.js
+++ src/datebase.js
@@ -1,3 +1,5 @@
+import { todayDate } from "./utils/today-data.js";
+
 export class Database {
   #database = {};
 
@@ -8,5 +10,28 @@ export class Database {
   insert(table, data) {
     if (Array.isArray(this.#database[table])) this.#database[table].push(data);
     else this.#database[table] = [data];
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+    const { completed_at, created_at } = this.#database[table][rowIndex];
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = {
+        id,
+        ...data,
+        completed_at,
+        created_at,
+        updated_at: todayDate(),
+      };
+    }
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1);
+    }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -34,4 +34,30 @@ export const routes = [
       return res.writeHead(201).end();
     },
   },
+  {
+    method: "PUT",
+    path: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+      const { title, description } = req.body;
+
+      database.update("tasks", id, {
+        title,
+        description,
+      });
+
+      return res.writeHead(204).end();
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      database.delete("tasks", id);
+
+      return res.writeHead(204).end();
+    },
+  },
 ];
